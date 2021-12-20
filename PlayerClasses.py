5c8d676e5de833d4f650c5a126dd447e2d61fd60
Sloont <colinabel93@gmail.com>
2021-12-20 09:54:13 -0500

    starting to work on the actual player classes. still need to make a common parent for the cards

---
+++ PlayerClasses.py
@@ -0,0 +1,8 @@
+class Adventurer:
+    def __init__(self):
+        self.health = 20
+        self.equipment = {
+            'hands': [],
+            'armor': None,
+            'amulet': None,
+        }
