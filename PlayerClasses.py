fb8405d980575b162fe5efa3ea341c9e5fb672a8
Sloont <colinabel93@gmail.com>
2021-12-21 09:20:09 -0500

    put some preliminary ideas into PlayerClasses.py. Just pushing them now for version control

--- PlayerClasses.py
+++ PlayerClasses.py
@@ -6,3 +6,20 @@ class Adventurer:
             'armor': None,
             'amulet': None,
         }
+        self.hand = []
+
+
+
+class Dungeon:
+    def __init__(self):
+        self.monsters = []
+        self.traps = []
+        self.hand = []
+
+
+class Encounter:
+    def __init__(self):
+        self.powerLevel = 0
+
+        self.adventurerBoard = None
+        self.dungeonBoard = []
