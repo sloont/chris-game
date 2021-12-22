46bc066b6404bab6ea148fb506c934178446f980
Sloont <ColinAbel93@gmail.com>
2021-12-21 22:41:38 -0500

    basic methods being worked on for Adventurer class. These will change. For now trying to get the ideas down.

--- PlayerClasses.py
+++ PlayerClasses.py
@@ -6,7 +6,30 @@ class Adventurer:
             'armor': None,
             'amulet': None,
         }
+        self.damage = 0
+        self.defense = 0
+
+        for item in self.equipment['hands']:
+            if item.type['type'] == 'damage':
+                self.damage += item.type['amount']
+            if item.type['type'] == 'defense':
+                self.defense += item.type['amount']
         self.hand = []
+
+    def modify_damage(self, amount):
+        self.damage += amount
+    def modify_health(self, amount):
+        self.health += amount
+    def modify_defense(self, amount):
+        self.defense += amount
+    def modify_swings(self):
+        pass
+    def roll_check(self):
+        pass
+    def toggle_item(self):
+        pass
+    def discard(self):
+
 
 
 
@@ -19,7 +42,7 @@ class Dungeon:
 
 class Encounter:
     def __init__(self):
-        self.powerLevel = 0
+        self.power_level = 0
 
-        self.adventurerBoard = None
-        self.dungeonBoard = []
+        self.adventurer_board = None
+        self.dungeon_board = []
