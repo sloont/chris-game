a04c99693ebff4e019c2237f2e6c47e7e645c299
Sloont <colinabel93@gmail.com>
2021-12-23 10:10:37 -0500

    reshaped populateDecks.py as Deck.py and its child classes. We will see how this goes, but I think these should be classes so we can get methods for drawing, shuffling, etc

---
+++ Deck.py
@@ -0,0 +1,38 @@
+import random, csv
+
+from CardClasses import AdventurerCard, DungeonCard
+
+class Deck:
+    def __init__(self, file):
+        self.cards = []
+        self.file = file
+
+        
+
+class AdventurerDeck(Deck):
+    def __init__(self,file):
+
+        super().__init__(file)
+
+        with open(file, newline='') as deckCSV:
+            reader = csv.DictReader(deckCSV, delimiter=',')
+            for row in reader:
+                self.cards.append(AdventurerCard(row))
+
+    def deck_list(self):
+        for card in self.cards:
+            print(f'{card.name}\n{card.id}\n{card.type}\n{card.value}\n{card.description}\n\n')
+
+class DungeonDeck(Deck):
+    def __init__(self,file):
+
+        super().__init__(file)
+
+        with open(file, newline='') as deckCSV:
+            reader = csv.DictReader(deckCSV, delimiter=',')
+            for row in reader:
+                self.cards.append(DungeonCard(row))
+
+    def deck_list(self):
+        for card in self.cards:
+            print(f'{card.name}\n{card.id}\n{card.type}\n{card.damage}\n{card.health}\n{card.power}\n\n')
